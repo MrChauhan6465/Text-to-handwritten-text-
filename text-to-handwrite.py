44bc83313452417001273f70bf14cf9031add3b6
MrChauhan6465 <89354259+MrChauhan6465@users.noreply.github.com>
2022-10-06 22:25:38 +0530

    Update text-to-handwrite.py

--- text-to-handwrite.py
+++ text-to-handwrite.py
@@ -1,12 +1,12 @@
 import pywhatkit as pw 
 
 while True:
-    print("=====>Welcome to text to handwritting converter🙏🙏🙏")
-    text = input("=====>(type exit to quit)\nEnter the text that you want to convert it into hand written text \n😃😃")
+    print("\n\n=====>Welcome to text to handwritting converter🙏🙏🙏")
+    text = input("=====>(type exit to quit)\n\nEnter the text that you want to convert it into hand written text \n😃😃")
     
     if text=='exit':
-        print("===>>ThankYou boss come back later👍👍👍")
+        print("\n===>>ThankYou boss, come back later👍👍👍")
         break
     else:
         pw.text_to_handwriting(text,"res.png",[1,2,3])
-        print("Successfully converted your text😎😎😎")
+        print("\nSuccessfully converted your text😎😎😎")
